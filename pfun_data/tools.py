679171f422fd2fbd368ee45439aec66c9b866549
robbiec <rocapp@gmail.com>
2026-03-24 23:27:11 -0700

    merged in local changes (from nix thinkpad)

--- pfun_data/tools.py
+++ pfun_data/tools.py
@@ -13,9 +13,7 @@ import datetime
 # Setup log file path using UTC date
 log_dir = Path("logs")
 log_dir.mkdir(exist_ok=True)
-tstamp = datetime.datetime.now(
-    datetime.timezone.utc
-).strftime('%Y-%m-%d-%H_%M')
+tstamp = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%d-%H_%M")
 log_filename = log_dir / f"{tstamp}.log"
 
 logging.config.dictConfig(
@@ -37,26 +35,28 @@ logging.config.dictConfig(
                 "filename": str(log_filename),
                 "mode": "a",
                 "encoding": "utf-8",
-            }
+            },
         },
         "loggers": {
-            "": {
-                "handlers": ["default", "file"],
-                "level": "INFO",
-                "propagate": True
-            }
+            "": {"handlers": ["default", "file"], "level": "INFO", "propagate": True}
         },
     }
 )
 
 
-def _extract_single_zipitem(zip_path, info_item, output_path, skip_existing: bool = True):
+def _extract_single_zipitem(
+    zip_path, info_item, output_path, skip_existing: bool = True
+):
     try:
         full_output_filepath = Path(output_path).joinpath(info_item.filename)
         # skip if flag is True AND file exists AND filesize is the same as expected uncompressed.
-        if all([full_output_filepath.exists(),
+        if all(
+            [
+                full_output_filepath.exists(),
                 skip_existing is True,
-                full_output_filepath.stat().st_size == info_item.file_size]):
+                full_output_filepath.stat().st_size == info_item.file_size,
+            ]
+        ):
             logging.info("* Skipping existing file '%s' !", full_output_filepath)
             return True
         logging.info("Extracting '%s'...", info_item.filename)
@@ -67,21 +67,23 @@ def _extract_single_zipitem(zip_path, info_item, output_path, skip_existing: boo
         sys.exit(0)
 
 
-def extractFromZip(zip_path: Path | str, output_path: Path | str, skip_existing: bool = True):
+def extractFromZip(
+    zip_path: Path | str, output_path: Path | str, skip_existing: bool = True
+):
     """Extract all files from a zipfile."""
     infolist = []
     with zipfile.ZipFile(zip_path, "r") as zipref:
         infolist = [zitem for zitem in zipref.infolist()]
     with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
         futures = [
-            executor.submit(_extract_single_zipitem, zip_path,
-                            info_item, output_path, skip_existing)
+            executor.submit(
+                _extract_single_zipitem, zip_path, info_item, output_path, skip_existing
+            )
             for info_item in infolist
         ]
         for future in concurrent.futures.as_completed(futures):
             if future.exception():
-                logging.error(
-                    f"Error extracting zipfile: {future.exception()}")
+                logging.error(f"Error extracting zipfile: {future.exception()}")
     logging.info("...done extracting zip.")
 
 
@@ -90,7 +92,9 @@ def _convert_single_csv_to_parquet(
 ) -> None:
     """Helper function to convert a single CSV file to a Parquet file."""
     try:
-        parquet_file = parquet_path.joinpath(csv_file.parent.name, csv_file.stem + ".parquet")
+        parquet_file = parquet_path.joinpath(
+            csv_file.parent.name, csv_file.stem + ".parquet"
+        )
         if all([skip_existing is True, parquet_file.exists()]):
             logging.warning("* Skipping existing file: '%s' !", str(parquet_file))
             return
@@ -132,8 +136,10 @@ def convertCsvToParquet(
     with concurrent.futures.ProcessPoolExecutor(max_workers=4) as executor:
         futures = [
             executor.submit(
-                _convert_single_csv_to_parquet, csv_file, parquet_path,
-                skip_existing=skip_existing
+                _convert_single_csv_to_parquet,
+                csv_file,
+                parquet_path,
+                skip_existing=skip_existing,
             )
             for csv_file in csv_files
         ]
@@ -141,14 +147,17 @@ def convertCsvToParquet(
             if future.exception():
                 logging.error(
                     "Error converting a CSV file to Parquet: %s",
-                    str(future.exception())
+                    str(future.exception()),
                 )
 
 
 class Csv2ParquetPipeline:
     def __init__(
-        self, zip_path: str | Path, csv_path: str | Path, parquet_path: str | Path,
-        skip_existing: bool = True
+        self,
+        zip_path: str | Path,
+        csv_path: str | Path,
+        parquet_path: str | Path,
+        skip_existing: bool = True,
     ):
         self.zip_path = zip_path
         self.csv_path = csv_path
@@ -171,15 +180,20 @@ class Csv2ParquetPipeline:
         return self
 
     def extract_csvs_from_zip(self):
-        return extractFromZip(self.zip_path, self.csv_path, skip_existing=self.skip_existing)
+        return extractFromZip(
+            self.zip_path, self.csv_path, skip_existing=self.skip_existing
+        )
 
     def convert_csvs_to_parquet(self):
-        return convertCsvToParquet(self.csv_path, self.parquet_path, skip_existing=self.skip_existing)
+        return convertCsvToParquet(
+            self.csv_path, self.parquet_path, skip_existing=self.skip_existing
+        )
 
 
 def unzipCsv2Parquet(zip_path, csv_path, parquet_path, skip_existing=True):
     pipeline = Csv2ParquetPipeline(
-        zip_path, csv_path, parquet_path, skip_existing=skip_existing)
+        zip_path, csv_path, parquet_path, skip_existing=skip_existing
+    )
     pipeline()
 
 
@@ -187,8 +201,7 @@ def main():
     parser = argparse.ArgumentParser(
         description="Extract CSVs from a ZIP file and convert them to Parquet format."
     )
-    parser.add_argument("--zip-path", required=True,
-                        help="Path to the input ZIP file.")
+    parser.add_argument("--zip-path", required=True, help="Path to the input ZIP file.")
     parser.add_argument(
         "--csv-path",
         required=True,
@@ -204,7 +217,7 @@ def main():
         required=False,
         help="Skip existing CSV files.",
         action="store_true",
-        default=True
+        default=True,
     )
     parser.add_argument(
         "--no-skip-existing",
@@ -220,8 +233,10 @@ def main():
         f"\n=== Press any key to confirm (\033[91mor CTRL+C to exit early\033[0m). ====\n\n{parsed_args_fmt}"
     )
     unzipCsv2Parquet(
-        parsed_args.zip_path, parsed_args.csv_path, parsed_args.parquet_path,
-        skip_existing=parsed_args.skip_existing
+        parsed_args.zip_path,
+        parsed_args.csv_path,
+        parsed_args.parquet_path,
+        skip_existing=parsed_args.skip_existing,
     )
 
 
